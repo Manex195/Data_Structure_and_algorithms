c605ea4059a9b9103913eaa66f1a8a393f02bab3
Manex195 <manexsangma03@gmail.com>
2023-10-31 02:28:36 +0600

    added iterators

--- src/queue.java
+++ src/queue.java
@@ -1,14 +1,27 @@
+import java.util.Iterator;
+
 // queue using linked list.
-public class queue {
+public class queue<T> implements Iterable<T> {
+    public Iterator<T> iterator() { return new QIterator(); }
+    private class QIterator implements Iterator<T> {
+        private Node current = first;
+        public boolean hasNext() { return current != null; }
+        public void remove() { /* if needed */ }
+        public T next() {
+            T item = current.element;
+            current = current.next;
+            return item;
+        }
+    }
     private class Node {
-        int element;
+        T element;
         Node next;
     }
     private Node first, last;
     public boolean isEmpty() {
         return first == null;
     }
-    public void enQueue(int element) {
+    public void enQueue(T element) {
         Node oldLast = last;
         last = new Node();
         last.element = element;
@@ -16,8 +29,8 @@ public class queue {
         if (isEmpty()) first = last;
         else oldLast.next = last;
     }
-    public int deQueue() {
-        int element = first.element;
+    public T deQueue() {
+        T element = first.element;
         first = first.next;
         if (isEmpty()) last = null;
         return element;
